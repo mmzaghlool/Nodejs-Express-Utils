2d73f3184b010bce9d08ab683bf57a4a8af9b9e5
Mostafa Mahmoud <mmzaghlool52@gmail.com>
2021-12-27 16:46:06 +0200

    1.1.25: add general encryption and decreption

--- src/sql/utils.ts
+++ src/sql/utils.ts
@@ -38,7 +38,7 @@ export function parseQuery(query: string, params: paramsType, schema: DatabaseSc
         }
     }
 
-    if (RegExp('&[a-zA-Z.]+', 'g').test(query)) {
+    if (RegExp(' &[a-zA-Z.]+', 'g').test(query)) {
         let exec = RegExp('&[a-zA-Z.]+', 'g').exec(query);
         console.log('exec: ', exec);
 
@@ -50,9 +50,7 @@ export function parseQuery(query: string, params: paramsType, schema: DatabaseSc
         }
     }
 
-    console.log(`RegExp(#'([a-zA-Z]+)', 'g').test(query)`, RegExp(`#'([a-zA-Z]+)'`, 'g').test(query));
-
-    if (RegExp(`#'([a-zA-Z]+)'`, 'g').test(query)) {
+    if (RegExp(` #'([a-zA-Z]+)'`, 'g').test(query)) {
         let exec = RegExp(`#'([a-zA-Z]+)'`, 'g').exec(query);
         console.log('exec: ', exec);
 
